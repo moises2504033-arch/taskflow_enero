fd90a40959ea109c1532435f654b575f778fa3c0
moises2504033 <moises2504033@hybridge.education>
2026-01-27 19:47:05 -0600

    27/01/2026

--- app.py
+++ app.py
@@ -1,1 +1,54 @@
 print ("¡Hola, mundo!")
+
+from flask import Flask, render_template, request, redirect, url_for
+from src.database import DBManager
+from src.modelos import Tarea, Proyectos 
+
+
+app = Flask(__name__)
+
+db_manager = DBManager()
+
+@app.route('/')
+def index():
+    
+    tareas_pendientes = db_manager.obtener_tareas(estado='pendiente')
+
+    proyectos = db_manager.obtener_proyectos()
+
+    return render_template('index.html', tareas=tareas_pendientes, proyectos=proyectos)
+
+@app.route('/crear', methods=['GET', 'POST'])
+def crear_tarea_web():
+
+    proyectos = db_manager.obtener_proyectos()
+
+    if request.method = 'POST':
+        titulo = request.form.get['titulo']
+        descripcion = request.form.get['descripcion']
+        limite = request.form.get['fecha_limite']
+        prioridad = request.form.get['prioridad']
+        proyecto_id = int(request.form.get['proyecto_id'])
+
+        nueva_tarea = Tarea(
+            titulo=titulo
+            descripcion=descripcion
+            fecha_limite=limite
+            prioridad=prioridad
+            proyecto_id=proyecto_id
+        )
+
+
+        db_manager.crear_tarea(nueva_tarea)
+        return redirect(url_for('index'))
+    
+    return render_template('formulario_tarea.html', proyectos=proyectos)
+
+
+if __name__ == '__main__':
+    db_manager.crear_tabla()
+    app.run(debug=True)
+
+        
+
+
